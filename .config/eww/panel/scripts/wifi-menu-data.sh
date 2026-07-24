3ca29f872c8885b476949c28790d57c206ade24b
Desmond Edem <edemintech@gmail.com>
2026-07-24 18:01:04 +0100

    feat(ui): add visual indicator for connected wifi network

--- .config/eww/panel/scripts/wifi-menu-data.sh
+++ .config/eww/panel/scripts/wifi-menu-data.sh
@@ -3,15 +3,16 @@
 # Fetch the wifi list, format as JSON for eww
 # nmcli -t -f SSID,SIGNAL,SECURITY dev wifi list
 
-nmcli -t -f SSID,SIGNAL,SECURITY dev wifi list | awk -F':' '
+nmcli -t -f IN-USE,SSID,SIGNAL,SECURITY dev wifi list | awk -F':' '
 BEGIN {
     print "["
     first = 1
 }
 {
-    ssid = $1
-    signal = $2
-    security = $3
+    in_use = ($1 == "*") ? "true" : "false"
+    ssid = $2
+    signal = $3
+    security = $4
     
     # skip empty SSIDs
     if (ssid == "") next
@@ -25,7 +26,7 @@ BEGIN {
     }
     first = 0
     
-    printf "  {\"ssid\": \"%s\", \"signal\": \"%s\", \"security\": \"%s\"}", ssid, signal, security
+    printf "  {\"in_use\": %s, \"ssid\": \"%s\", \"signal\": \"%s\", \"security\": \"%s\"}", in_use, ssid, signal, security
 }
 END {
     print "\n]"
